4954bb0e91c726b1a99a5d34eb5eaa0675d06de1
Rick de Pauw <rick@griffid.com>
2022-07-01 08:46:55 +0200

    Updated AboutForm.

--- MiniBug/AboutForm.Designer.cs
+++ MiniBug/AboutForm.Designer.cs
@@ -40,7 +40,7 @@
             // 
             // btOK
             // 
-            this.btOK.Location = new System.Drawing.Point(315, 146);
+            this.btOK.Location = new System.Drawing.Point(315, 160);
             this.btOK.Name = "btOK";
             this.btOK.Size = new System.Drawing.Size(75, 23);
             this.btOK.TabIndex = 0;
@@ -50,11 +50,10 @@
             // 
             // lblApplicationName
             // 
-            this.lblApplicationName.AutoSize = true;
             this.lblApplicationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblApplicationName.Location = new System.Drawing.Point(107, 12);
             this.lblApplicationName.Name = "lblApplicationName";
-            this.lblApplicationName.Size = new System.Drawing.Size(197, 24);
+            this.lblApplicationName.Size = new System.Drawing.Size(319, 24);
             this.lblApplicationName.TabIndex = 1;
             this.lblApplicationName.Text = "MiniBug Issue Tracker";
             // 
@@ -70,38 +69,35 @@
             // 
             // lblVersion
             // 
-            this.lblVersion.AutoSize = true;
             this.lblVersion.Location = new System.Drawing.Point(107, 52);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(46, 13);
+            this.lblVersion.Size = new System.Drawing.Size(319, 13);
             this.lblVersion.TabIndex = 3;
             this.lblVersion.Text = "Version";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(107, 81);
+            this.label2.Location = new System.Drawing.Point(107, 78);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 13);
+            this.label2.Size = new System.Drawing.Size(319, 43);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Copyright © 2019 - João Martiniano";
+            this.label2.Text = "Copyright © 2019 - João Martiniano\r\nForked 2022 by RickZeeland";
             // 
             // linkLabel1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(107, 110);
+            this.linkLabel1.Location = new System.Drawing.Point(107, 124);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(265, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(319, 13);
             this.linkLabel1.TabIndex = 5;
             this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "https://www.github.com/joaomartiniano/MiniBug";
+            this.linkLabel1.Text = "https://github.com/RickZeeland/MiniBug.git";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 181);
+            this.ClientSize = new System.Drawing.Size(438, 201);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblVersion);
