59a83d7757b4aeb49ffc627599f7b2c939bf73bc
RickZeeland <rick@griffid.com>
2022-07-11 13:16:50 +0200

    Updated AboutForm

--- MiniBug/AboutForm.Designer.cs
+++ MiniBug/AboutForm.Designer.cs
@@ -42,9 +42,10 @@
             // 
             // btOK
             // 
-            this.btOK.Location = new System.Drawing.Point(327, 205);
+            this.btOK.Location = new System.Drawing.Point(339, 250);
+            this.btOK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btOK.Name = "btOK";
-            this.btOK.Size = new System.Drawing.Size(75, 23);
+            this.btOK.Size = new System.Drawing.Size(87, 30);
             this.btOK.TabIndex = 0;
             this.btOK.Text = "OK";
             this.btOK.UseVisualStyleBackColor = true;
@@ -53,16 +54,17 @@
             // lblApplicationName
             // 
             this.lblApplicationName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApplicationName.Location = new System.Drawing.Point(107, 12);
+            this.lblApplicationName.Location = new System.Drawing.Point(125, 16);
             this.lblApplicationName.Name = "lblApplicationName";
-            this.lblApplicationName.Size = new System.Drawing.Size(319, 24);
+            this.lblApplicationName.Size = new System.Drawing.Size(372, 31);
             this.lblApplicationName.TabIndex = 1;
             this.lblApplicationName.Text = "MiniBug Issue Tracker";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(20, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(23, 16);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(64, 64);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -71,25 +73,25 @@
             // 
             // lblVersion
             // 
-            this.lblVersion.Location = new System.Drawing.Point(107, 52);
+            this.lblVersion.Location = new System.Drawing.Point(125, 68);
             this.lblVersion.Name = "lblVersion";
-            this.lblVersion.Size = new System.Drawing.Size(319, 13);
+            this.lblVersion.Size = new System.Drawing.Size(372, 17);
             this.lblVersion.TabIndex = 3;
             this.lblVersion.Text = "Version";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(107, 78);
+            this.label2.Location = new System.Drawing.Point(125, 102);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(319, 43);
+            this.label2.Size = new System.Drawing.Size(372, 47);
             this.label2.TabIndex = 4;
             this.label2.Text = "Copyright © 2019 - João Martiniano\r\nForked 2022 by RickZeeland";
             // 
             // linkLabel1
             // 
-            this.linkLabel1.Location = new System.Drawing.Point(107, 108);
+            this.linkLabel1.Location = new System.Drawing.Point(125, 141);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(353, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(372, 29);
             this.linkLabel1.TabIndex = 5;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/RickZeeland/MiniBug.git";
@@ -97,26 +99,27 @@
             // 
             // linkLabelIcons8
             // 
-            this.linkLabelIcons8.Location = new System.Drawing.Point(316, 150);
+            this.linkLabelIcons8.Location = new System.Drawing.Point(314, 196);
             this.linkLabelIcons8.Name = "linkLabelIcons8";
-            this.linkLabelIcons8.Size = new System.Drawing.Size(144, 13);
+            this.linkLabelIcons8.Size = new System.Drawing.Size(177, 27);
             this.linkLabelIcons8.TabIndex = 6;
             this.linkLabelIcons8.TabStop = true;
             this.linkLabelIcons8.Text = "https://icons8.com";
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(107, 150);
+            this.label1.Location = new System.Drawing.Point(103, 196);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(319, 21);
+            this.label1.Size = new System.Drawing.Size(213, 27);
             this.label1.TabIndex = 7;
-            this.label1.Text = "Pie chart and Clipboard icon by Icons8";
+            this.label1.Text = "Pie chart and Clipboard icon by";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // AboutForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(460, 240);
+            this.ClientSize = new System.Drawing.Size(503, 294);
             this.Controls.Add(this.linkLabelIcons8);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
@@ -125,8 +128,9 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblApplicationName);
             this.Controls.Add(this.btOK);
-            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
